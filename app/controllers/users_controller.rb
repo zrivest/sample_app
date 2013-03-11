945267e17b22d33d6b7a6a39881dcf750e1c8ff3
zrivest@gmail.com <zrivest@gmail.com>
2013-03-11 18:02:52 -0500

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,17 +9,9 @@ class UsersController < ApplicationController
   end
 
   def create
-  	@user = User.new(params[:user])
-  	if @user.save
-  		redirect_to @user
-  	else
-  		render 'new'
-  	end
-  end
-
-  def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to my badass website!"
       redirect_to @user
     else
