3e54c073d41a7c13a495bce66c4f905ded153ec0
zrivest@gmail.com <zrivest@gmail.com>
2013-03-17 12:44:09 -0500

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,9 +26,20 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
+  it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
 
   describe "when name is not present" do
     before { @user.name = " " }
@@ -136,18 +147,18 @@ describe User do
     end
   end
 
-    describe "email address with mixed case" do
-       let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+  describe "email address with mixed case" do
+     let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
 
-       it "should be saved as all lower-case" do
-         @user.email = mixed_case_email
-         @user.save
-         @user.reload.email.should == mixed_case_email.downcase
-       end
-    end
+     it "should be saved as all lower-case" do
+       @user.email = mixed_case_email
+       @user.save
+       @user.reload.email.should == mixed_case_email.downcase
+     end
+  end
 
-    describe "remember token" do
-      before { @user.save }
-      its(:remember_token) { should_not be_blank }
-    end            	
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end            	
 end
