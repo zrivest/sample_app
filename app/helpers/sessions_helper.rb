f84e1b8e86ddd8f215de82675b95cee7d27ef4ee
zrivest@gmail.com <zrivest@gmail.com>
2013-03-28 09:54:52 -0500

    Add user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -21,6 +21,13 @@ module SessionsHelper
       user == current_user
     end
 
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
 	def destroy
     	sign_out
     	redirect_to root_url
